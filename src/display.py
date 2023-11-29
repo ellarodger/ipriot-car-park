2c614429910967504872fd6e7e34c1c2ca7b75cb
Ella Rodger <ellarodger@outlook.com.au>
2023-11-30 01:19:29 +0800

    Added constructors and attributes to the display and sensor class

--- src/display.py
+++ src/display.py
@@ -1,2 +1,15 @@
 class Display:
-    ...
+    def __int__(self,
+                id,
+                car_park,
+                message="",
+                is_on=False):
+        self.id = id
+        self.car_park = car_park
+        self.message = message or []
+        self.is_on = is_on or []
+
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}"
+
