2efd67951bc25c654ef0bf929185830c52138699
Ella Rodger <ellarodger@outlook.com.au>
2023-12-03 16:07:11 +0800

    Added logging functionality

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,7 @@
 from display import Display
 from sensor import Sensor
+from pathlib import Path
+from datetime import datetime
 
 
 class CarPark:
@@ -9,14 +11,16 @@ class CarPark:
                 max_bays,
                 plates=None,
                 sensors=None,
-                displays=None):
+                displays=None,
+                log_file=Path("log.txt"),):
         self.location = location
         self.name = name
         self.max_bays = max_bays
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
-
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
@@ -30,10 +34,12 @@ class CarPark:
     def add_vehicle(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_vehicle(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     def update_displays(self):
         for display in self.displays:
@@ -49,5 +55,8 @@ class CarPark:
             return 0
         return self.max_bays - len(self.plates)
 
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as file:
+            file.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
     def __str__(self):
         return f"{self.name} at {self.location} with a capacity of {self.max_bays} bays"
--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -65,12 +65,12 @@ class TestCarPark(unittest.TestCase):
                               "CPP Melonbrook",
                               100,
                               log_file="new_log.txt")
-        self.car_park.add_car("NEW-001")
+        self.car_park.add_vehicle("NEW-001")
         with self.car_park.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn(last_line, "NEW-001")  # check plate entered
-        self.assertIn(last_line, "entered")  # check description
-        self.assertIn(last_line, "\n")  # check entry has a new line
+        self.assertIn("NEW-001", last_line)  # check plate entered
+        self.assertIn("entered", last_line)  # check description
+        self.assertIn("\n", last_line)  # check entry has a new line
 
     def test_car_logged_when_exiting(self):
         # TODO: change this to use a class attribute or new instance variable
@@ -82,9 +82,9 @@ class TestCarPark(unittest.TestCase):
         self.car_park.remove_vehicle("NEW-001")
         with self.car_park.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn(last_line, "NEW-001")  # check plate entered
-        self.assertIn(last_line, "exited")  # check description
-        self.assertIn(last_line, "\n")  # check entry has a new line
+        self.assertIn("NEW-001", last_line)  # check plate entered
+        self.assertIn("exited", last_line)  # check description
+        self.assertIn("\n", last_line)  # check entry has a new line
 
 
 if __name__ == "__main__":
